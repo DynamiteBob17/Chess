dfb9f28d3d9efb53050f627393cf47c1ffe0cfe3
DynamiteBob17 <jan.mlinaric@gmail.com>
2024-03-17 18:48:41 +0100

    feat: introduce castling to the board

--- src/main/java/hr/mlinx/chess/board/Board.java
+++ src/main/java/hr/mlinx/chess/board/Board.java
@@ -1,9 +1,9 @@
 package hr.mlinx.chess.board;
 
+import hr.mlinx.chess.util.ChessDialog;
 import hr.mlinx.chess.util.FENParser;
 import hr.mlinx.chess.util.SoundPlayer;
 
-import javax.swing.*;
 import java.awt.*;
 import java.util.Arrays;
 import java.util.Map;
@@ -14,6 +14,11 @@ public class Board {
     private LastMove lastMove;
     private final SoundPlayer soundPlayer;
     private final Map<Integer, Image> pieceImagesRegular;
+
+    private boolean whiteCanCastleKingside = true;
+    private boolean whiteCanCastleQueenside = true;
+    private boolean blackCanCastleKingside = true;
+    private boolean blackCanCastleQueenside = true;
 
     public Board(SoundPlayer soundPlayer, Map<Integer, Image> pieceImagesRegular) {
         chessBoard = new int[8][8];
@@ -62,9 +67,10 @@ public class Board {
     }
 
     private void makeMove(Move move, boolean isSimulation) {
-        int colorMakingMove = Piece.getColorFromPiece(getPieceAt(move.fromRow, move.fromCol));
+        int pieceToMove = getPieceAt(move.fromRow, move.fromCol);
+        int colorMakingMove = Piece.getColorFromPiece(pieceToMove);
 
-        setPieceAt(move.toRow, move.toCol, getPieceAt(move.fromRow, move.fromCol));
+        setPieceAt(move.toRow, move.toCol, pieceToMove);
         setPieceAt(move.fromRow, move.fromCol, Piece.NONE);
 
         if (move.getSpecialMove() == SpecialMove.EN_PASSANT) {
@@ -72,40 +78,46 @@ public class Board {
             int capturedPawnCol = move.toCol;
 
             setPieceAt(capturedPawnRow, capturedPawnCol, Piece.NONE);
+            return;
         }
 
         if (!isSimulation && move.getSpecialMove() == SpecialMove.PAWN_PROMOTION) {
-            int promotedPiece = showPromotionDialog(colorMakingMove);
+            int promotedPiece = ChessDialog.showPromotionDialog(colorMakingMove, pieceImagesRegular);
             setPieceAt(move.toRow, move.toCol, promotedPiece);
-        }
-    }
-
-    private int showPromotionDialog(int promotingColor) {
-        ImageIcon[] options = new ImageIcon[4];
-        int pieceTypeIndex = 0;
-
-        for (int pieceType : new int[]{Piece.QUEEN, Piece.ROOK, Piece.BISHOP, Piece.KNIGHT}) {
-            options[pieceTypeIndex++] = new ImageIcon(pieceImagesRegular.get(pieceType | promotingColor));
+            return;
         }
 
-        int n = JOptionPane.showOptionDialog(null,
-                "Choose promotion:",
-                "Pawn Promotion",
-                JOptionPane.YES_NO_CANCEL_OPTION,
-                JOptionPane.QUESTION_MESSAGE,
-                null,
-                options,
-                options[0]);
+        if (move.getSpecialMove() == null) { // Regular move, not castling
+            int movedPieceType = Piece.getTypeFromPiece(pieceToMove);
 
-        // Mapping remains the same as before...
-        int promotedPieceType = switch (n) {
-            case 1 -> Piece.ROOK;
-            case 2 -> Piece.BISHOP;
-            case 3 -> Piece.KNIGHT;
-            default -> Piece.QUEEN;
-        };
+            switch (movedPieceType) {
+                case Piece.KING -> {
+                    if (colorMakingMove == Piece.WHITE) {
+                        whiteCanCastleKingside = false;
+                        whiteCanCastleQueenside = false;
+                    } else {
+                        blackCanCastleKingside = false;
+                        blackCanCastleQueenside = false;
+                    }
+                }
+                case Piece.ROOK -> {
+                    switch (move.fromRow * 10 + move.fromCol) {
+                        case 70 -> whiteCanCastleQueenside = false;
+                        case 77 -> whiteCanCastleKingside = false;
+                        case 0 -> blackCanCastleQueenside = false;
+                        case 7 -> blackCanCastleKingside = false;
+                    }
+                }
+            }
+        }
 
-        return promotedPieceType | promotingColor;
+        if (move.getSpecialMove() == SpecialMove.SHORT_CASTLE) {
+            if (colorMakingMove == Piece.WHITE) {
+                setPieceAt(7, 5, Piece.ROOK | Piece.WHITE);
+                setPieceAt(7, 7, Piece.NONE);
+            }
+            // ... similar logic for black
+        }
     }
 
     public int[][] getChessBoard() {
@@ -128,5 +140,67 @@ public class Board {
     public LastMove getLastMove() {
         return lastMove;
     }
+
+    public boolean whiteCanCastleKingside() {
+        return whiteCanCastleKingside;
+    }
+
+    public boolean whiteCanCastleQueenside() {
+        return whiteCanCastleQueenside;
+    }
+
+    public boolean blackCanCastleKingside() {
+        return blackCanCastleKingside;
+    }
+
+    public boolean blackCanCastleQueenside() {
+        return blackCanCastleQueenside;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int[] row : chessBoard) {
+            for (int piece : row) {
+                stringBuilder.append(pieceDescriptor(piece)).append(" ");
+            }
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    private String pieceDescriptor(int piece) {
+        switch (piece) {
+            case Piece.NONE:
+                return ".";
+            case Piece.WHITE | Piece.PAWN:
+                return "WP";
+            case Piece.WHITE | Piece.KNIGHT:
+                return "WN";
+            case Piece.WHITE | Piece.BISHOP:
+                return "WB";
+            case Piece.WHITE | Piece.ROOK:
+                return "WR";
+            case Piece.WHITE | Piece.QUEEN:
+                return "WQ";
+            case Piece.WHITE | Piece.KING:
+                return "WK";
+            case Piece.BLACK | Piece.PAWN:
+                return "BP";
+            case Piece.BLACK | Piece.KNIGHT:
+                return "BN";
+            case Piece.BLACK | Piece.BISHOP:
+                return "BB";
+            case Piece.BLACK | Piece.ROOK:
+                return "BR";
+            case Piece.BLACK | Piece.QUEEN:
+                return "BQ";
+            case Piece.BLACK | Piece.KING:
+                return "BK";
+            default:
+                return "?";
+        }
+    }
+
 
 }
